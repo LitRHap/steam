773436916fd1241217d1e47f748f3422d299bb4d
LitRHap <73509282+LitRHap@users.noreply.github.com>
2022-01-17 22:35:53 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const SteamUser = require('steam-user');
+
+const user = new SteamUser();
+
+const logOnOptions = {
+	accountName: 'alifputrasteam',  //Enter here your account login
+	password: 'Tangguh11'		 //Enter your password
+}
+
+user.logOn(logOnOptions);
+
+user.on('loggedOn', () => {
+	console.log(logOnOptions.accountName + ' - Successfully logged on');
+	user.setPersona(1);                 //1 - online, 7 - invisible
+	user.gamesPlayed([440,730,570,204450,550]);    //List app IDs
+});
