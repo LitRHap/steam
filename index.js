4054f078f099ea41f75e4b443b34ba26d7287b7a
LitRHap <73509282+LitRHap@users.noreply.github.com>
2022-04-04 08:29:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ user.logOn(logOnOptions);
 user.on('loggedOn', () => {
 	console.log(logOnOptions.accountName + ' - Successfully logged on');
 	user.setPersona(1);                 //1 - online, 7 - invisible
-	user.gamesPlayed([730,440,578080,570,204450,550,1325860,1160780,227940,431960,385800]);    //List app IDs
+	user.gamesPlayed([730,440,578080,570,204450,550,1325860,1160780,227940,431960,385800,1212620]);    //List app IDs
 });
